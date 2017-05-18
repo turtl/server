5f20fbb26afc79749b9ced744210bc205698bab5
Andrew Danger Lyon <orthecreedence@gmail.com>
2017-05-17 23:23:25 -0700

    add feedback endpoint

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,3 +1,15 @@
+var tres = require('../helpers/tres');
+var model = require('../models/feedback');
+
 exports.route = function(app) {
+	app.post('/feedback', send_feedback);
+};
+
+var send_feedback = function(req, res) {
+	var data = req.body;
+	var user_id = req.user.id;
+	var username = req.user.username;
+	var client = req.header('X-Turtl-Client');
+	tres.wrap(res, model.send(user_id, username, client, data));
 };
 
--- models/feedback.js
+++ models/feedback.js
@@ -1,2 +1,28 @@
 "use strict";
 
+var email_model = require('./email');
+var error = require('../helpers/error');
+var config = require('../helpers/config');
+var analytics = require('./analytics');
+
+exports.send = function(user_id, username, client, data) {
+	var body = data.body || false;
+	if(!body) return Promise.reject(error.bad_request('no feedback given'));
+	var subject = 'New Turtl feedback from '+username+' ('+user_id+')';
+	var email_body = [
+		'You have received feedback from '+username+' (user id '+user_id+', client '+client+'):',
+		'',
+		'************',
+		'',
+		body,
+	];
+	return email_model.send(username, config.app.emails.admin, subject, email_body.join('\n'))
+		.then(function() {
+			analytics.track(user_id, 'feedback.send', {client: client});
+			return true;
+		})
+		.catch(function(err) {
+			throw error.internal('problem sending confirmation email: '+err.message);
+		});
+};
+
