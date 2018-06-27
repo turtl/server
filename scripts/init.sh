595e1125874ad17259bf612c75a843264f6fa57f
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-06-27 06:30:59 -0700

    init script fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 if [ "${TURTL_SERVER_PLUGIN_REPO}" != "" ]; then
 	if [ "${TURTL_SERVER_PLUGIN_LOCATION}" == "" ]; then
-		TURTL_SERVER_PLUGIN_LOCATION="plugins_"
+		TURTL_SERVER_PLUGIN_LOCATION="plugins"
 	fi
 	git clone ${TURTL_SERVER_PLUGIN_REPO} "${TURTL_SERVER_PLUGIN_LOCATION}" || \
 		{ echo "Error grabbing plugins"; exit 1; }
