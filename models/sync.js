0fa41a1575d7d15356e9a31826d78ecdaea8890a
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-09-23 19:38:18 -0700

    changing connection cutoff time again (30s -> 15s) kinda lame. oh well, doesnt materially affect much.

--- models/sync.js
+++ models/sync.js
@@ -318,7 +318,11 @@ exports.sync_from = function(user_id, from_sync_id, poll) {
 	if(!from_sync_id && from_sync_id !== 0) {
 		return Promise.reject(error.bad_request('missing `sync_id` var: '+JSON.stringify(from_sync_id)));
 	}
-	var cutoff = (new Date().getTime()) + (1000 * 30);
+	// after some testing, 15s seems to be about the max that various clients
+	// will support. which is funny, because we control the clients. but they do
+	// not do as they are asked. they do as they please (mainly talknig about
+	// the core, which has a 90s timeout for this call).
+	var cutoff = (new Date().getTime()) + (1000 * 15);
 	return poll_sync_items(user_id, from_sync_id, poll, cutoff)
 		.then(function(sync_records) {
 			return link_sync_records(sync_records);
